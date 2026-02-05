e9904a61e9ab76d6df489353e80655e761601946
mvntaha <syedamuntahapk@gmail.com>
2026-02-05 21:05:41 +0500

    inserted fields in brand model

--- Models/Masters/Brand.cs
+++ Models/Masters/Brand.cs
@@ -22,6 +22,22 @@ namespace Yash_Gems___Jewelleries.Models.Masters
         [StringLength(500)]
         public string? Description { get; set; }
 
+        public string? LogoUrl { get; set; }
+
+        [StringLength(200)]
+        public string? Website { get; set; }
+
+        [StringLength(200)]
+        public string? Location { get; set; }
+
+        [EmailAddress]
+        [StringLength(100)]
+        public string? Email { get; set; }
+
+        [Phone]
+        [StringLength(20)]
+        public string? PhoneNumber { get; set; }
+
         public bool IsActive { get; set; } = true;
 
         public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
